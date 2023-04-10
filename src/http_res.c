9f3a90d5dde3a9227812f74c4493517b3b9182d8
Rahul Gupta <rahulr09gupta@gmail.com>
2023-04-10 21:27:30 +0530

    feat: add response and basic templating utils

--- src/http_res.c
+++ src/http_res.c
@@ -1,1 +1,76 @@
-#include "http_req.h"
+#include "http_res.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *render_template(const char *template, placeholder_t *placeholders, size_t num_placeholders)
+{
+    char *result = NULL;
+    const char *start = template;
+    const char *end = NULL;
+    int offset = 0;
+
+    for (size_t i = 0; i < num_placeholders; i++)
+    {
+        const char *placeholder = placeholders[i].placeholder;
+        const char *value = placeholders[i].value;
+        int len = strlen(placeholder);
+        int value_len = strlen(value);
+
+        while ((end = strstr(start, placeholder)) != NULL)
+        {
+            int new_len = end - start + value_len;
+            result = (char *)realloc(result, offset + new_len + 1);
+            memcpy(result + offset, start, end - start);
+            memcpy(result + offset + (end - start), value, value_len);
+            offset += new_len;
+            start = end + len;
+        }
+    }
+
+    if (start != template + strlen(template))
+    {
+        int new_len = template + strlen(template) - start;
+        result = (char *)realloc(result, offset + new_len + 1);
+        memcpy(result + offset, start, new_len);
+        offset += new_len;
+    }
+
+    result = (char *)realloc(result, offset + 1);
+    result[offset] = '\0';
+    return result;
+}
+
+char *render_template_file(const char *filepath, placeholder_t *placeholders, size_t num_placeholders)
+{
+    char *template = NULL;
+    char *result = NULL;
+    FILE *file = fopen(filepath, "r");
+
+    if (file == NULL)
+    {
+        fprintf(stderr, "Failed to open template file '%s'\n", filepath);
+        return NULL;
+    }
+
+    // Get the size of the file
+    fseek(file, 0, SEEK_END);
+    size_t filesize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Allocate memory for the template
+    template = (char *)malloc(filesize + 1);
+
+    // Read the file into the buffer
+    fread(template, 1, filesize, file);
+    fclose(file);
+    template[filesize] = '\0';
+
+    // Call the existing render_template function with the buffer and the placeholders
+    result = render_template(template, placeholders, num_placeholders);
+
+    // Free the template buffer
+    free(template);
+
+    return result;
+}
--- src/http_res.h
+++ src/http_res.h
@@ -1,6 +1,43 @@
 #ifndef http_res_h
 #define http_res_h
 
-// This module can be responsible for preparing the http response
+#include <stdlib.h>
+
+typedef struct
+{
+
+} http_res;
+
+typedef enum
+{
+    HTTP_STATUS_CONTINUE = 100,
+    HTTP_STATUS_OK = 200,
+    HTTP_STATUS_CREATED = 201,
+    HTTP_STATUS_ACCEPTED = 202,
+    HTTP_STATUS_NO_CONTENT = 204,
+    HTTP_STATUS_BAD_REQUEST = 400,
+    HTTP_STATUS_UNAUTHORIZED = 401,
+    HTTP_STATUS_FORBIDDEN = 403,
+    HTTP_STATUS_NOT_FOUND = 404,
+    HTTP_STATUS_METHOD_NOT_ALLOWED = 405,
+    HTTP_STATUS_INTERNAL_SERVER_ERROR = 500,
+    HTTP_STATUS_NOT_IMPLEMENTED = 501,
+    HTTP_STATUS_BAD_GATEWAY = 502,
+    HTTP_STATUS_SERVICE_UNAVAILABLE = 503,
+    HTTP_STATUS_GATEWAY_TIMEOUT = 504
+} http_status_t;
+
+typedef struct
+{
+    char *placeholder;
+    char *value;
+} placeholder_t;
+
+char *http_res_render(int count, ...);
+void http_res_status(http_status_t status);
+
+// utility functions to render templates dynamically
+char *render_template(const char *template, placeholder_t *placeholders, size_t num_placeholders);
+char *render_template_file(const char *filepath, placeholder_t *placeholders, size_t num_placeholders);
 
 #endif /* http_res_h */
