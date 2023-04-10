3d1c47f510f7e199080d771b909ff6db0f586bbd
Rahul Gupta <rahulr09gupta@gmail.com>
2023-04-10 21:25:09 +0530

    refactor: remove handle incoming conn from server

--- src/server.c
+++ src/server.c
@@ -5,8 +5,7 @@
 
 typedef struct server server_t;
 
-server_t *server_init(int domain, int service, int protocol, u_long interface,
-                      int port, int backlog, void (*handle_incoming_connections)(server_t *server))
+server_t *server_init(int domain, int service, int protocol, u_long interface, int port, int backlog)
 {
     server_t *server = malloc(sizeof(server_t));
     server->domain = domain;
@@ -20,8 +19,6 @@ server_t *server_init(int domain, int service, int protocol, u_long interface,
     server->address.sin_family = domain;
     server->address.sin_port = htons(port);
     server->address.sin_addr.s_addr = htonl(interface);
-
-    server->handle_incoming_connections = handle_incoming_connections;
 
     server->socket_fd = socket(domain, service, protocol);
     if (server->socket_fd == 0)
@@ -45,7 +42,7 @@ server_t *server_init(int domain, int service, int protocol, u_long interface,
     return server;
 }
 
-void server_handle_connections(server_t *server)
+void server_free(server_t *server)
 {
-    server->handle_incoming_connections(server);
+    free(server);
 }
--- src/server.h
+++ src/server.h
@@ -16,17 +16,11 @@ typedef struct server
     int backlog;
     int socket_fd;
     struct sockaddr_in address;
-
-    void (*handle_incoming_connections)(struct server *server);
 } server_t;
 
 server_t *server_init(int domain, int service, int protocol, u_long interface,
-                      int port, int backlog, void (*handle_incoming_connections)(server_t *server));
+                      int port, int backlog);
 
-/*
- * This function calls the handle_incoming_connections passed
- * to the server_init function.
- */
-void server_handle_connections(server_t *server);
+void server_free(server_t *server);
 
 #endif /* server_h */
