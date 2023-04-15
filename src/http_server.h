e8f5e2c9034de21c633f31495a54709b46f0cae0
Rahul Gupta <rahulr09gupta@gmail.com>
2023-04-15 16:53:06 +0530

    feat: add thread_pool field in http_server

--- src/http_server.h
+++ src/http_server.h
@@ -2,6 +2,7 @@
 #define http_server_h
 
 #include "../external/include/owl/collections/hashmap.h"
+#include "../external/include/owl/systems/thread_pool.h"
 #include "http_req.h"
 #include "http_res.h"
 #include "server.h"
@@ -11,10 +12,13 @@ typedef struct http_server
     server_t *server;
     owl_hashmap_t *routes;
     owl_hashmap_t *http_status_map;
+    owl_thread_pool_t *thread_pool;
+    char *static_files_path;
 } http_server_t;
 
 http_server_t *http_server_init(int port, int backlog);
 int register_route(http_server_t *server, char *uri, char *(*handler)(http_req_t *req, http_res_t *res));
+void register_static(http_server_t *server, char *path);
 
 /*
  * Start listening for incoming connections
