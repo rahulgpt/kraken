30c65c2656bdc491dcec555a93a63789f8931498
Rahul Gupta <rahulr09gupta@gmail.com>
2023-04-16 23:06:08 +0530

    feat: add examples in main.c

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
 #include "owl/utils/log.h"
 #include "src/http_req.h"
+#include "src/http_res.h"
 #include "src/http_server.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
-#define PROTOCOL 0
 #define PORT 8000
 #define BACKLOG 10
 
-char *home_handler(http_req_t *req)
+char *home_handler(http_req_t *req, http_res_t *res)
 {
-    owl_println("Method: %d", req->method);
-    owl_println("Home Handler called!!");
-    return "HTTP/1.1 200 OK\r\n\r\n<html><head><title>Kraken Server</title></head><body><h2 style=\"color:lightcoral;\">Home Page</h2></body><html>";
+    res_status(res, 200);
+    res_content_type(res, "text/html");
+
+    placeholder_t placeholders[] = {
+        {"{{title}}", "Welcome to the Home Page!"},
+        {"{{content}}", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget est nibh. Sed auctor, diam elementum interdum vehicula, lectus urna euismod arcu, ut feugiat odio arcu et dui. Cras id sodales neque, accumsan luctus ante. Nam sed ullamcorper enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur rhoncus nulla at pharetra feugiat. Mauris vel odio erat. Donec finibus turpis sit amet suscipit vestibulum. Donec at urna elementum, pellentesque ante eget, congue nisi. Praesent enim lectus, facilisis sit amet turpis quis, rhoncus venenatis turpis. Donec porta tellus in mauris consequat, vitae cursus diam commodo. Fusce ullamcorper luctus sagittis. Morbi lacus odio, accumsan non rutrum non, consectetur vel tortor. "},
+        {"{{something1}}", "10:26 PM"},
+    };
+
+    return res_render_template_file("./template.html", placeholders, NUM_PLACEHOLDERS(placeholders));
 }
 
-char *about_handler(http_req_t *req)
+char *about_handler(http_req_t *req, http_res_t *res)
 {
-    owl_println("Method: %d", req->method);
-    owl_println("About Handler called!!");
-    return "HTTP/1.1 200 OK\r\n\r\n<html><head><title>Kraken Server</title></head><body><h2 style=\"color:lightcoral;\">About Page</h2></body><html>";
+    return "<h1>About Page</h1><a href=\"/home\">Home</a>";
+}
+
+char *index_handler(http_req_t *req, http_res_t *res)
+{
+    return res_render_static_file("./react/react.html");
 }
 
 int main()
@@ -30,6 +41,11 @@ int main()
 
     register_route(server, "/home", home_handler);
     register_route(server, "/about", about_handler);
+    register_route(server, "/", index_handler);
+
+    register_static(server, "public");
+    register_static(server, "react");
+    register_static(server, "next");
 
     http_server_listen(server);
     http_server_free(server);
