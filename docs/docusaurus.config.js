380da2d907d438044c78068bf2306ff943098776
Rahul Gupta <rahulr09gupta@gmail.com>
2023-04-18 11:30:52 +0530

    docs: fix build errors due to bad baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: process.env.url || "https://krakendocs.netlify.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
